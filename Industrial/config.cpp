ac3319a6b059e3b1fc6d7c28c82030ac3fa2ff4c
Alevaric <96006040+Alevaric@users.noreply.github.com>
2025-04-27 14:55:10 +0100

    Fixed naming conventions

--- Industrial/config.cpp
+++ Industrial/config.cpp
@@ -12,7 +12,7 @@ class CfgVehicles
 {
 	class HouseNoDestruct;
 
-	class Land_Red_Barn: HouseNoDestruct
+	class Land_House_Barn: HouseNoDestruct
 	{
 		scope=1;
 		model="ALV_UN_Structures_Industrial\Industrial\House_Barn.p3d";
@@ -117,7 +117,7 @@ class CfgVehicles
 			};
 		};
 	};
-	class Land_Workshop_Medium: HouseNoDestruct
+	class Land_House_Workshop: HouseNoDestruct
 	{
 		scope=1;
 		model="ALV_UN_Structures_Industrial\Industrial\House_Workshop.p3d";
@@ -197,7 +197,7 @@ class CfgVehicles
 			};
 		};
 	};
-	class Land_Stable_Store: HouseNoDestruct
+	class Land_House_Stable_Shop: HouseNoDestruct
 	{
 		scope=1;
 		model="ALV_UN_Structures_Industrial\Industrial\House_Stable_Shop.p3d";
@@ -315,7 +315,7 @@ class CfgVehicles
 			};
 		};
 	};
-	class Land_Wood_Workshop: HouseNoDestruct
+	class Land_House_Wood_Workshop: HouseNoDestruct
 	{
 		scope=1;
 		model="ALV_UN_Structures_Industrial\Industrial\House_Wood_Workshop.p3d";
@@ -458,7 +458,7 @@ class CfgVehicles
 			};
 		};
 	};
-	class Land_Blacksmiths: HouseNoDestruct
+	class Land_House_Blacksmiths: HouseNoDestruct
 	{
 		scope=1;
 		model="ALV_UN_Structures_Industrial\Industrial\House_Blacksmiths.p3d";
@@ -601,7 +601,7 @@ class CfgVehicles
 			};
 		};
 	};
-	class Land_Wooden_Stable: HouseNoDestruct
+	class Land_House_Wooden_Stable: HouseNoDestruct
 	{
 		scope=1;
 		model="ALV_UN_Structures_Industrial\Industrial\House_Wooden_Stable.p3d";
@@ -706,12 +706,12 @@ class CfgVehicles
 			};
 		};
 	};
-	class Land_StorageHouse: HouseNoDestruct
+	class Land_House_StorageHouse: HouseNoDestruct
 	{
 		scope=1;
 		model="ALV_UN_Structures_Industrial\Industrial\House_StorageHouse.p3d";
 	};
-	class Land_Storehouse_1: HouseNoDestruct
+	class Land_House_Storehouse_1: HouseNoDestruct
 	{
 		scope=1;
 		model="ALV_UN_Structures_Industrial\Industrial\House_Storehouse_1.p3d";
@@ -829,7 +829,7 @@ class CfgVehicles
 			};
 		};
 	};
-	class Land_ShoeHorseBarn: HouseNoDestruct
+	class Land_House_ShoeHorseBarn: HouseNoDestruct
 	{
 		scope=1;
 		model="ALV_UN_Structures_Industrial\Industrial\House_ShoeHorseBarn.p3d";
@@ -948,7 +948,7 @@ class CfgVehicles
 			};
 		};
 	};
-	class Land_PigShed: HouseNoDestruct
+	class Land_House_PigShed: HouseNoDestruct
 	{
 		scope=1;
 		model="ALV_UN_Structures_Industrial\Industrial\House_PigShed.p3d";
@@ -1011,7 +1011,7 @@ class CfgVehicles
 			};
 		};
 	};
-	class Land_PigShed_2: HouseNoDestruct
+	class Land_House_PigShed_2: HouseNoDestruct
 	{
 		scope=1;
 		model="ALV_UN_Structures_Industrial\Industrial\House_PigShed_2.p3d";
@@ -1074,7 +1074,7 @@ class CfgVehicles
 			};
 		};
 	};
-	class Land_MediumBarn: HouseNoDestruct
+	class Land_House_MediumBarn: HouseNoDestruct
 	{
 		scope=1;
 		model="ALV_UN_Structures_Industrial\Industrial\House_MediumBarn.p3d";
@@ -1218,7 +1218,7 @@ class CfgVehicles
 			};
 		};
 	};
-	class Land_LargeBarn: HouseNoDestruct
+	class Land_House_LargeBarn: HouseNoDestruct
 	{
 		scope=1;
 		model="ALV_UN_Structures_Industrial\Industrial\House_LargeBarn.p3d";
@@ -1362,7 +1362,7 @@ class CfgVehicles
 			};
 		};
 	};
-	class Land_Small_Construction: HouseNoDestruct
+	class Land_House_Small_Construction: HouseNoDestruct
 	{
 		scope=1;
 		model="ALV_UN_Structures_Industrial\Industrial\House_Small_Construction.p3d";
@@ -1425,10 +1425,145 @@ class CfgVehicles
 			};
 		};
 	};
-	class Land_Medium_Construction: HouseNoDestruct
+	class Land_House_Medium_Construction: HouseNoDestruct
 	{
 		scope=1;
 		model="ALV_UN_Structures_Industrial\Industrial\House_Medium_Construction.p3d";
+		class DamageSystem
+		{
+			class GlobalHealth
+			{
+				class Health
+				{
+					hitpoints=1000;
+				};
+			};
+			class GlobalArmor
+			{
+				class Projectile
+				{
+					class Health
+					{
+						damage=0;
+					};
+					class Blood
+					{
+						damage=0;
+					};
+					class Shock
+					{
+						damage=0;
+					};
+				};
+				class Melee
+				{
+					class Health
+					{
+						damage=0;
+					};
+					class Blood
+					{
+						damage=0;
+					};
+					class Shock
+					{
+						damage=0;
+					};
+				};
+				class FragGrenade
+				{
+					class Health
+					{
+						damage=0;
+					};
+					class Blood
+					{
+						damage=0;
+					};
+					class Shock
+					{
+						damage=0;
+					};
+				};
+			};
+		};
+	};
+
+	class Land_House_StorageStableDouble: HouseNoDestruct
+	{
+		scope=1;
+		model="ALV_UN_Structures_Industrial\Industrial\House_StorageStableDouble.p3d";
+	};
+	class Land_House_StorageStable_1: HouseNoDestruct
+	{
+		scope=1;
+		model="ALV_UN_Structures_Industrial\Industrial\House_StorageStable_1.p3d";
+	};
+	class Land_House_StorageStable_2: HouseNoDestruct
+	{
+		scope=1;
+		model="ALV_UN_Structures_Industrial\Industrial\House_StorageStable_2.p3d";
+	};
+
+	class Land_House_LargeStable: HouseNoDestruct
+	{
+		scope=1;
+		model="ALV_UN_Structures_Industrial\Industrial\House_LargeStable.p3d";
+		class Doors
+		{
+			class Doors1
+			{
+				displayName="door 1";
+				component="doors1";
+				soundPos="doors1_action";
+				animPeriod=1;
+				initPhase=0;
+				initOpened=0.30000001;
+				soundOpen="doorWoodFrontOpen";
+				soundClose="doorWoodFrontClose";
+				soundLocked="doorWoodFrontRattle";
+				soundOpenABit="doorWoodFrontOpenABit";
+			};
+			class Doors2
+			{
+				displayName="door 2";
+				component="doors2";
+				soundPos="doors2_action";
+				animPeriod=1;
+				initPhase=0;
+				initOpened=0.30000001;
+				soundOpen="doorWoodFrontOpen";
+				soundClose="doorWoodFrontClose";
+				soundLocked="doorWoodFrontRattle";
+				soundOpenABit="doorWoodFrontOpenABit";
+			};
+			class Doors3
+			{
+				displayName="door 3";
+				component="doors3";
+				soundPos="doors3_action";
+				animPeriod=1;
+				initPhase=0;
+				initOpened=0.30000001;
+				soundOpen="doorWoodFrontOpen";
+				soundClose="doorWoodFrontClose";
+				soundLocked="doorWoodFrontRattle";
+				soundOpenABit="doorWoodFrontOpenABit";
+			};
+			class Doors4
+			{
+				displayName="door 4";
+				component="doors4";
+				soundPos="doors4_action";
+				animPeriod=1;
+				initPhase=0;
+				initOpened=0.30000001;
+				soundOpen="doorMetalSlideLargeOpen";
+				soundClose="doorMetalSlideLargeClose";
+				soundLocked="doorMetalSlideLargeRattle";
+				soundOpenABit="doorMetalSlideLargeOpenABit";
+			};
+		};
 		class DamageSystem
 		{
 			class GlobalHealth
